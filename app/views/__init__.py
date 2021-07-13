a06cc41b13965ef6708422e45eb0269766be689f
gustavodm <gusttavomota@gmail.com>
2021-07-13 18:52:47 -0300

    temporary views blueprint

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,2 +1,6 @@
 from .notices_view import Notices
+from flask import Flask
 from .test_view import Test
+from app.views.users_view import bp as bp_users
+def init_app(app: Flask):
+    app.register_blueprint(bp_users)
