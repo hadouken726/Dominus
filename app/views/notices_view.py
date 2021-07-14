194d9f9679091c6a5f182a04182b179639e452a3
matheus <matheus2hcosta@gmail.com>
2021-07-13 21:06:17 -0300

    feature: Post Notice, need jwt

--- app/views/notices_view.py
+++ app/views/notices_view.py
@@ -1,13 +1,21 @@
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from flask import request
+
 from app.models.notices_model import NoticesModel
 from http import HTTPStatus
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 import sqlalchemy.exc as e
+
+from app.settings.database import db
+
 
 class Notices(Resource):
     def get(self, notice_id=None):
         if notice_id is None:
             notices = NoticesModel()
             query = notices.query.all()
+            print(query)
             return {"Notices": [
                 {"Title": notices.title, "Description": notices.desc,
                  "Updated_at": notices.updated_at.strftime("%d/%m/%Y")}
@@ -27,4 +35,21 @@ class Notices(Resource):
             except AttributeError:
                 return {"Message": "Invalid number, just accept int with register ids",
                         "Error": "Attribute Error"}, HTTPStatus.BAD_REQUEST
-            
+
+    @jwt_required()
+    def post(self):
+        current_user = get_jwt_identity()
+        if "teste" == "teste":
+            try:
+                data = request.get_json(force=True)
+                parser = reqparse.RequestParser()
+                parser.add_argument("title", type=str, required=True)
+                parser.add_argument("desc", type=str, required=True)
+                args = parser.parse_args()
+                db.session.add(NoticesModel(**args))
+                db.session.commit()
+
+                return args
+
+            except:
+                return {"Error": " Devs is coding..."}
