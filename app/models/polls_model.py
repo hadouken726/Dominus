e97fbb8554e90b31c5193d012b7bae1b20893751
tkosudi <tkosudi@gmail.com>
2021-07-15 11:35:50 -0300

    PollsVotesModels was created

--- app/models/polls_model.py
+++ app/models/polls_model.py
@@ -14,4 +14,4 @@ class PollsModel(db.Model):
     end_at = Column(DateTime, default=(datetime.utcnow() + timedelta(days=2)))
     desc = Column(String, nullable=False)
 
-    id_poll_vote = db.relationship("PollVotesModel", backref=backref("polls_id"))
+    votes = db.relationship("PollsVotesModel", backref=backref("poll"))
