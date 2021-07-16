bbc92126d0004c2faabe6c07277d461f1ccf67cc
gustavodm <gusttavomota@gmail.com>
2021-07-16 00:14:11 -0300

    add '/' to routes endpoints

--- app/settings/api.py
+++ app/settings/api.py
@@ -8,15 +8,15 @@ def init_app(app: Flask):
     JWTManager(app)
 
     from app.views import Notices
-    api.add_resource(Notices, "/notices", endpoint="notices", methods=["GET", "POST"])
-    api.add_resource(Notices, "/notices/<notice_id>", endpoint="notices", methods=["GET", "DELETE", "PATCH"])
+    api.add_resource(Notices, "/notices", endpoint="/notices", methods=["GET", "POST"])
+    api.add_resource(Notices, "/notices/<notice_id>", endpoint="/notices", methods=["GET", "DELETE", "PATCH"])
 
 
     from app.views import Login
-    api.add_resource(Login, "/login", endpoint="login", methods=["POST"])
+    api.add_resource(Login, "/login", endpoint="/login", methods=["POST"])
 
 
     from app.views import Users
-    api.add_resource(Users, "/signup", endpoint="users", methods=["POST"])
-    api.add_resource(Users, "/users", endpoint="users", methods=["GET"])
-    api.add_resource(Users, "/users/<user_id>", endpoint="users", methods=["GET", "DELETE", "PATCH"])
+    api.add_resource(Users, "/signup", endpoint="/users", methods=["POST"])
+    api.add_resource(Users, "/users", endpoint="/users", methods=["GET"])
+    api.add_resource(Users, "/users/<user_id>", endpoint="/users", methods=["GET", "DELETE", "PATCH"])
