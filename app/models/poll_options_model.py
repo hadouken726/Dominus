ec3303efafc22a549eedc9c763095bada09bf426
gustavodm <gusttavomota@gmail.com>
2021-07-16 18:22:53 -0300

    feature: add marshall to models

--- app/models/poll_options_model.py
+++ app/models/poll_options_model.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import backref, relationship
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import DateTime, Integer, String, Text
-from app.settings.database import db
+from app.settings.database import db, ma
 from sqlalchemy.schema import Column
 from datetime import datetime, timedelta
+from marshmallow_sqlalchemy.schema import auto_field
 
 
 class PollOptionsModel(db.Model):
@@ -12,3 +13,10 @@ class PollOptionsModel(db.Model):
     id = Column(Integer, primary_key=True)
     poll_id = Column(Integer, ForeignKey('polls.id'))
     name = Column(String(100), nullable=False)
+
+class PollOptionSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = PollOptionsModel
+        load_instance = True
+        ordered = True
+    id = auto_field('id', dump_only=True)
--- app/models/polls_model.py
+++ app/models/polls_model.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import backref, relationship
 from sqlalchemy.sql.sqltypes import DateTime, Integer, String, Text
-from app.settings.database import db
+from app.settings.database import db, ma
 from sqlalchemy.schema import Column
 from datetime import datetime, timedelta
-
+from marshmallow_sqlalchemy import auto_field
 
 class PollsModel(db.Model):
     __tablename__ = "polls"
@@ -15,7 +15,12 @@ class PollsModel(db.Model):
     desc = Column(String(1000), nullable=False)
     title = Column(String(50), nullable=False)
 
-
+class PollSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = PollsModel
+        load_instance = True
+        ordered = True
+    id = auto_field('id', dump_only=True)
 
 
     
