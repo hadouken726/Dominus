2e0dad06625ecc4c94f2e3e67528963dfdb14484
gustavodm <gusttavomota@gmail.com>
2021-07-15 22:46:37 -0300

    refactor: relocation of folders

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,14 +1,9 @@
-from .notices_view import Notices
-from users_view import bp as bp_users
-from flask import Flask, Blueprint
-
-
-def init_app(app: Flask):
-
-    from app.views.login_view import Login
-    from app.views.notices_view import Notices
-    from app.views.test_view import Test
-    from app.views.users_view import Users
-
-from app.views.test_view import Test
+from app.views.login_view import Login
+from app.views.notices_view import Notices
 from app.views.users_view import Users
+
+
+
+
+
+
