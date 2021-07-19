ca08c0dc4afcc2e73ce08f82bd049e569db16b8b
gustavodm <gusttavomota@gmail.com>
2021-07-19 10:56:09 -0300

    feat: add GET method to route /events

--- app/services/events_service.py
+++ app/services/events_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.sql.expression import update
 from sqlalchemy.sql.functions import current_user
-from app.models.events_invitations_model import EventInvitationSchema
+from app.models.events_invitations_model import EventInvitationSchema, EventsInvitationsModel
 from http import HTTPStatus
 from flask_jwt_extended import get_jwt_identity
 from flask import current_app, request
@@ -96,7 +96,13 @@ class EventsService:
         abort(HTTPStatus.UNAUTHORIZED, message='Only admin user or event host can delete this event')
 
     def get_all(self):
-        pass
+        all_events = EventsModel.query.all()
+        events_hosted_by_current_user = list(EventsModel.query.filter_by(host_id=self.current_user.id))
+        accepted_invitations = EventsInvitationsModel.query.filter(EventsInvitationsModel.guest_id == self.current_user.id, EventsInvitationsModel.status == 'accepted')
+        events_in = [invitation.event for invitation in accepted_invitations]
+        if self.current_user.is_admin:
+            return all_events
+        return EventSchema().dump(events_hosted_by_current_user + events_in, many=True)
 
 
         
--- app/views/events_view.py
+++ app/views/events_view.py
@@ -11,7 +11,7 @@ class Events(Resource):
         return response
 
     def get(self):
-        print(request.args)
+        return EventsService(3).get_all()
 
     def patch(self, event_id):
         request_data = request.get_json()
@@ -21,6 +21,6 @@ class Events(Resource):
 
     def delete(self, event_id):
         events_service = EventsService(3)
-        response = events_service.delete(events_service)
+        response = events_service.delete(event_id)
         return response
     
