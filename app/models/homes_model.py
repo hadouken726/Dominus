6198b7cc15427c2d22572d4173b9dba4283a6a0b
matheus <matheus2hcosta@gmail.com>
2021-07-19 19:31:51 -0300

    create struture of homes files and routes, and get

--- app/models/homes_model.py
+++ app/models/homes_model.py
@@ -1,8 +1,11 @@
+from flask_marshmallow import fields
 from marshmallow_sqlalchemy.schema import auto_field
 from sqlalchemy.orm import backref, relation, relationship
 from sqlalchemy.sql.functions import mode
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import DateTime, Float, Integer, String
+
+from app.models.users_model import UserSchema
 from app.settings.database import db, ma
 from sqlalchemy.schema import Column
 from datetime import datetime
@@ -22,4 +25,6 @@ class HomeSchema(ma.SQLAlchemyAutoSchema):
         model = HomesModel
         load_instance = True
         ordered = True
+
     id = auto_field('id', dump_only=True)
+    residents = ma.Nested(UserSchema, many=True, exclude=("id", "cpf", "is_admin", "is_home_in_possession"))
