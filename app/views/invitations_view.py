a7b10fc8b9417458e79df5db76669b0987608bc2
gustavodm <gusttavomota@gmail.com>
2021-07-20 14:48:02 -0300

    feat: add GET method to routes /invitations and /invitations/:id

--- app/views/invitations_view.py
+++ app/views/invitations_view.py
@@ -1,14 +1,23 @@
 from flask_restful import Resource
 from app.services.invitations_service import InvitationsService
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 class Invitations(Resource):
-    def get(self):
-        pass
+    @jwt_required()
+    def get(self, invitation_id=None):
+        current_user_id = get_jwt_identity()
+        invitation_service = InvitationsService(current_user_id)
+        if not invitation_id:
+            return invitation_service.get_all()
+        return invitation_service.get_one(invitation_id)
 
+    @jwt_required()
     def patch(self, invitation_id):
         invitations_service = InvitationsService(3)
-        response = invitations_service.patch(invitation_id)
-        return response
+        return invitations_service.patch(invitation_id)
+    
+
+    @jwt_required()    
     def delete(self, invitation_id):
         return InvitationsService(1).delete(invitation_id)
     
