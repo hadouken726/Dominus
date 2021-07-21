7db67f1f6a7e5760507f65e4cdea7065bb19dba1
gustavodm <gusttavomota@gmail.com>
2021-07-20 22:28:38 -0300

    feature: add fees resource

--- app/services/fees_service.py
+++ app/services/fees_service.py
@@ -1,3 +1,5 @@
+from functools import partial
+from app.models.fees_model import FeeSchema, FeesModel
 from sqlalchemy.sql.expression import update
 from sqlalchemy.sql.functions import current_user
 from app.models.events_invitations_model import EventInvitationSchema, EventsInvitationsModel
@@ -13,7 +15,7 @@ from marshmallow import Schema, fields
 
 
 
-class EventsService:
+class FeesService:
 
     
     def __init__(self, current_user_id) -> None:
@@ -22,18 +24,59 @@ class EventsService:
         self.session = current_app.db.session
 
 
+    def post(self, request_data):
+        if self.current_user.is_admin:
+            try:
+                fee_to_post = FeeSchema().load(request_data, session=self.session)
+                self.session.add(fee_to_post)
+                self.session.commit()
+            except ValidationError as VE:
+                abort(HTTPStatus.BAD_REQUEST, message=VE.messages)
+            return FeeSchema().dump(fee_to_post), HTTPStatus.CREATED
+        abort(HTTPStatus.UNAUTHORIZED, message='Access allowed only for admins!')
+    
 
-    def _deserialize(self, request_data: dict):
-        try:
-            return EventSchema().load(request_data, session=self.session)
-        except ValidationError as VE:
-            abort(HTTPStatus.BAD_REQUEST, message=VE.messages)
+    def patch(self, fee_id):
+        request_data = request.get_json()
+        fetched_fee = FeesModel.query.get_or_404(fee_id)
+        if self.current_user.is_admin:
+            try:
+                fee_to_patch = FeeSchema().load(request_data, session=self.session, instance=fetched_fee, partial=True)
+                self.session.add(fee_to_patch)
+                self.session.commit()
+            except ValidationError as VE:
+                abort(HTTPStatus.BAD_REQUEST, message=VE.messages)
+            return FeeSchema().dump(fee_to_patch), HTTPStatus.CREATED
+        abort(HTTPStatus.UNAUTHORIZED, message='Only admin can patch a fee!')
+
+    def delete(self, fee_id):
+        fee_to_delete = FeesModel.query.get_or_404(fee_id)
+        if self.current_user.is_admin:
+            self.session.delete(fee_to_delete)
+            self.session.commit()
+            return '', HTTPStatus.NO_CONTENT
+        abort(HTTPStatus.UNAUTHORIZED, message='Only admin can delete a fee!')
+
+    def get_all(self):
+        all_fees = FeesModel.query.all()
+        current_user_fees = FeesModel.query.filter_by(home_id=self.current_user.home_id)
+        if self.current_user.is_admin:
+            return FeeSchema().dump(all_fees, many=True), HTTPStatus.OK
+        return FeeSchema().dump(current_user_fees, many=True), HTTPStatus.OK
+
+    def get_one(self, fee_id):
+        fee = FeesModel.query.get_or_404(fee_id)
+        if self.current_user.is_admin or fee.home_id == self.current_user.id:
+            return FeeSchema().dump(fee), HTTPStatus.OK
+        abort(HTTPStatus.UNAUTHORIZED, message='Fee does not belong to the user')
 
         
-    def _get_all_other_users(self):
-        all_users = UsersModel.query.all()
-        all_users.remove(self.current_user)
-        return all_users
+        
+
+
+
+
+
 
     def _admin_post(self, new_event: EventsModel):
         all_other = self._get_all_other_users()
---
+++ app/views/fees_view.py
@@ -0,0 +1,31 @@
+from flask_restful import Resource
+from flask import request
+from app.services.fees_service import FeesService
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+class Fees(Resource):
+    @jwt_required()
+    def get(self, fee_id=None):
+        current_user_id = get_jwt_identity()
+        fees_service = FeesService(current_user_id)
+        if not fee_id:
+            return fees_service.get_all()
+        return fees_service.get_one(fee_id)
+
+    @jwt_required()
+    def patch(self, fee_id):
+        current_user_id = get_jwt_identity()
+        fees_service = FeesService(current_user_id)
+        return fees_service.patch(fee_id)
+    
+
+    @jwt_required()    
+    def delete(self, fee_id):
+        current_user_id = get_jwt_identity()
+        return FeesService(current_user_id).delete(fee_id)
+
+    @jwt_required()
+    def post(self):
+        fee_entry = request.get_json()
+        current_user_id = get_jwt_identity()
+        return FeesService(current_user_id).post(fee_entry)
