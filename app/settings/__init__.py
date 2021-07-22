e40905591c20149ec1598c964f61e6aa7f3bacad
gustavodm <gusttavomota@gmail.com>
2021-07-22 08:38:49 -0300

    refactor: extends token duration to 1 hour

--- app/settings/__init__.py
+++ app/settings/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from datetime import timedelta
 
 def init_app(app: Flask):
     from environs import Env
@@ -8,3 +8,4 @@ def init_app(app: Flask):
     env.read_env()
     app.config["JSON_SORT_KEYS"] = False
     app.config['JWT_SECRET_KEY'] = env('JWT_SECRET_KEY')
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
